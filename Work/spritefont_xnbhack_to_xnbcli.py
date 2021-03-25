4c757bea2e8f64bff3ef56097cc11e7b83222c6d
b-b-blueberry <pocketsathome@gmail.com>
2021-03-25 22:17:59 +1100

    Updated docstring to include more info and optional steps to deploy output files when running, as well as info on formatting any user-provided resource files. Replaced json with commentjson to allow C-style comments in resource files. Added system call to xnbcli to automatically pack and deploy after converting and generating output file. Changed int() to float() cast on glyph kerning. Rearranged content entries in output to match format expected by xnbcli. Script now orders output character map by unicode decimal index to prevent font corruption caused by unordered resource files (character is not available in this SpriteFont error).

--- Work/spritefont_xnbhack_to_xnbcli.py
+++ Work/spritefont_xnbhack_to_xnbcli.py
@@ -1,11 +1,43 @@
 """
 blueberry's quick StardewXNBHack -> xnbcli-packable spritefont converter
 
-Why this script?
-    StardewXNBHack exports fonts into an easily-editable MonoGame format,
-    while xnbcli expects fonts to be in a hybrid XNA/custom format with header data to repack them.
+Instructions on usage:
+    1.  Set 'target_dir' to some directory on your PC containing 1 font file.
+        If many fonts are in the same directory, only the first alphabetically will be used.
+    2.  Set 'header_filename' to the name of your provided xnbcli header file, instructions below.
+    3.  Set 'output_filename' to whatever you like to match your project.
+        The output file will expect a PNG spritefont image with the same name as 'output_filename'.
+        If you like, you can change the PNG path expected after the file is converted by opening and searching 'export'.
+    4.  Run this script!
 
+    (Optional:)
+
+    5a. Set 'xnbcli_dir' to some directory containing your install of xnbcli.
+        If set to a valid path, the 'out_fp' file will be created as normal,
+        then xnbcli will pack it and deploy the XNB file into the given 'output_dir'.
+
+    (Otherwise:)
+
+    5b. Copy the output file to your 'xnbcli/unpacked' folder along with the matching PNG spritefont image,
+        and run 'pack.bat' to pack it into an XNB file.
+        If it failed, the 'xnbcli/packed' directory will be empty, or there will be an XNB of ~10 bytes in size.
+    5c. Copy the packed XNB file to your mod folder and include it in your project.
+
+
+
+Instructions for providing a header file:
+    For this script, we automatically pack the xnbcli header data into the file to save on time doing it manually
+    each time you edit and pack the font. It's much easier.
+    1.  Unpack any spritefont through xnbcli
+        (place the SpriteFont.xnb in 'xnbcli/packed' and run 'unpack.bat')
+    2.  Remove the entire 'content' entry from the file and close braces.
+    3.  You should now be left with a valid JSON file containing some 'header' and 'readers' entries.
+        Save as some filename to match 'header_filename: str' below and place in your 'target_dir'.
+
+
+Why this script?
     If you're adding a custom spritefont to Stardew Valley you'll likely be adding it through the Content API, eg.
+
         public T Load<T>(IAssetInfo asset)
         {
             if (asset.AssetNameEquals(MySpriteFontAssetKey)
@@ -13,42 +45,84 @@ Why this script?
             return (T)(object) null
         }
 
-Instructions on usage:
-    1. Set 'target_dir' to some directory on your PC containing 1 font file.
-       If many fonts are in the same directory, only the first alphabetically will be used.
-    2. Set 'header_filename' to the name of your provided xnbcli header file, instructions below.
-    3. Set 'output_filename' to whatever you like to match your project.
-       The output file will expect a PNG spritefont image with the same name as 'output_filename'.
-       If you like, you can change the PNG path expected after the file is converted by opening and searching 'export'.
-    4. Run this script!
-    5. Copy the output file to your 'xnbcli/unpacked' folder along with the matching PNG spritefont image,
-       and run 'pack.bat' to pack it into an XNB file.
-       If it failed, the 'xnbcli/packed' directory will be empty, or there will be an XNB of ~10 bytes in size.
-    6. Copy the packed XNB file to your mod folder and include it in your project.
+    Since complete fonts are comprised of a definition (JSON) file and an image (PNG) file,
+    we aren't able to load a single font through a single call to Content.Load<SpriteFont>:
+    instead, we compile the font files into an XNB which is then automatically unpacked and loaded for us
+    by the content manager.
 
-Instructions for providing a header file:
-    For this script, we automatically pack the xnbcli header data into the file to save on time doing it manually
-    each time you edit and pack the font. It's much easier.
-    1. Unpack any spritefont through xnbcli
-       (place the SpriteFont.xnb in 'xnbcli/packed' and run 'unpack.bat')
-    2. Remove the entire 'content' entry from the file and close braces.
-    3. You should now be left with a valid JSON file containing some 'header' and 'readers' entries.
-       Save as some filename to match 'header_filename: str' below and place in your 'target_dir'.
+    StardewXNBHack exports fonts into a sensibly-organised and easily-editable MonoGame format,
+    while xnbcli expects fonts to be in a hybrid XNA/custom format with header data to repack them.
+    This script converts font definition files from a well-defined format into a working mess.
+
+
+Notes on input file format:
+    To save on brain problems, fonts shouldn't ever be made in the XNA/custom format.
+    It expects to be passed separate lists for 'bounds'/'cropping'/'kerning', with entries aligned with
+    the index of the character in 'characters'. This is both clinically insane and impossible to reasonably manage.
+
+    Instead, this program expects files to be formatted in the MonoGame/StardewXNBHack format:
+
+        {
+            "LineSpacing": 26,
+            "Spacing": 0,
+            "DefaultCharacter": "~",
+            "Glyphs":
+            {
+                // Numerical
+                "1":
+                {
+                  "BoundsInTexture": "{X:24 Y:0 Width:12 Height:20}",
+                  "Cropping": "{X:0 Y:0 Width:12 Height:26}",
+                  "Character": "1",
+                  "LeftSideBearing": 0.0,
+                  "Width": 12.0,
+                  "RightSideBearing": 0.0,
+                  "WidthIncludingBearings": 12.0
+                },
+                // Alphabetical
+                "A":
+                {
+                  "BoundsInTexture": "{X:24 Y:0 Width:12 Height:20}",
+                  "Cropping": "{X:0 Y:0 Width:12 Height:26}",
+                  "Character": "A",
+                  "LeftSideBearing": 0.0,
+                  "Width": 12.0,
+                  "RightSideBearing": 0.0,
+                  "WidthIncludingBearings": 12.0
+                }
+            }
+        }
+
+    Formatting notes:
+        -   The 'Characters' field is NOT required in the source file.
+            The output file 'characterMap' list is populated with keys from your 'Glyphs' dictionary.
+            You can happily omit making any separate list similar to 'Characters' altogether.
+        -   The 'Character' field in each 'Glyphs' entry is NOT required in the source file.
+            This can be omitted also for the same reason.
+        -   Being a dictionary, each character in 'Glyphs' should only be defined once.
+        -   Following MonoGame, values in serialised dictionary values should not be comma-separated, nor have precision.
+        -   Other numerical values may be given as precise decimals.
+        -   No trailing commas should be used throughout.
+        -   C-style comments are accepted, but not preserved.
+        -   The order of entries in the source file, or the order of each character in 'Glyphs', does not matter.
+            Your entries will be ordered in the output file as expected by xnbcli/XNA.
 """
 
 import os
-import json
+import commentjson
 import glob
 import pathlib
 
 encoding = "utf-8"  # Spritefont should use "utf-8" for special characters. "utf-8-sig" apparently causes SyntaxError.
 texture_format = 0  # Only tested values 0 and 5, seems to be unimportant.
 
-target_dir: str = os.path.normpath("E:/Dev/Projects/SDV/SpritefontXnbHackToXnbcli")  # Substitute with your working dir.
-out_filename: str = "output.json"     # Name of font will always be this value. No batch operations; one font at a time.
+xnbcli_dir: str = os.path.normpath("E:/Dev/Projects/SDV/XNBCLI/xnbcli.exe")  # Optional; to automatically deploy font.
+target_dir: str = os.path.normpath("E:/Dev/Projects/SDV/SpritefontXnbHackToXnbcli")  # Working source file directory.
+output_dir: str = os.path.normpath("E:/Dev/Projects/SDV/Projects/InteractiveSchedule/InteractiveSchedule/assets")
+out_filename: str = "output.json"  # Name of font will always be this value. No batch operations; one font at a time.
 header_filename: str = "header.json"  # Files given to xnbcli to be packed require some standard headers.
 out_fp: str = os.path.join(target_dir, out_filename)
-header_fp: str= os.path.join(target_dir, header_filename)
+header_fp: str = os.path.join(target_dir, header_filename)
 
 
 # Form dictionaries from Glyphs field dict values formatted as strings, converting values to integers
@@ -67,22 +141,22 @@ def main():
 
     # Read file containing header data from any game spritefont unpacked through xnbcli previously
     with open(file=header_fp, mode="r", encoding=encoding) as header_file:
-        header_js = json.loads(header_file.read())
+        header_js = commentjson.loads(header_file.read())
 
         # Read StardewXNBHack unpacked font file
         with open(file=in_fp, mode="r", encoding=encoding) as in_file:
-            in_js = json.loads(in_file.read())
+            in_js = commentjson.loads(in_file.read())
 
             # Parse MonoGame-formatted fields into XNA/XNB format
             kerning = []
             cropping = []
             glyphs = []
-            for index, character in enumerate(in_js["Characters"]):
-                try:
-                    glyph = in_js["Glyphs"][character]
-                except KeyError:
-                    # Skip characters with missing values: character will remain in characterMap/Characters.
-                    continue
+
+            # Characters must be sorted by ascending unicode decimal in the output file
+            source_glyph_keys_sorted = sorted(in_js["Glyphs"].keys(), key=str)
+
+            for character in source_glyph_keys_sorted:
+                glyph = in_js["Glyphs"][character]
                 bounds_field = field_to_str_int_dict(glyph["BoundsInTexture"])
                 glyphs.append({
                     "x": bounds_field["X"],
@@ -98,9 +172,9 @@ def main():
                     "height": crop_field["Height"]
                 })
                 kerning.append({
-                    "x": int(glyph["LeftSideBearing"]),
-                    "y": int(glyph["RightSideBearing"]),
-                    "z": int(glyph["Width"]),
+                    "x": float(glyph["LeftSideBearing"]),
+                    "y": float(glyph["RightSideBearing"]),
+                    "z": float(glyph["Width"]),
                 })
 
             out_stem = pathlib.Path(out_fp).stem  # Stem is used for font image file, assuming filenames are identical.
@@ -110,20 +184,24 @@ def main():
             # Order of fields seemingly matters here to xnbcli: 'texture' must be the first entry in 'content'.
             out_js["content"]["texture"] = {"format": texture_format, "export": out_stem + ".png"}
 
-            # Populate readily-available fields
+            out_js["content"]["glyphs"] = glyphs
+            out_js["content"]["cropping"] = cropping
+            out_js["content"]["characterMap"] = source_glyph_keys_sorted
             out_js["content"]["verticalLineSpacing"] = in_js["LineSpacing"]
             out_js["content"]["horizontalSpacing"] = in_js["Spacing"]
-            out_js["content"]["defaultCharacter"] = in_js["DefaultCharacter"]
-            out_js["content"]["characterMap"] = in_js["Characters"]
-
-            # Populate nested fields
-            out_js["content"]["glyphs"] = glyphs
             out_js["content"]["kerning"] = kerning
-            out_js["content"]["cropping"] = cropping
+            out_js["content"]["defaultCharacter"] = in_js["DefaultCharacter"]
 
             # Write output to file
             with open(file=out_fp, mode="w", encoding=encoding) as out_file:
-                json.dump(obj=out_js, fp=out_file, indent=4)
+                commentjson.dump(obj=out_js, fp=out_file, indent=4)
+
+            # Run xnbcli to pack font file and deploy XNB to output directory
+            if os.path.exists(xnbcli_dir):
+                os.system(str.format("{} pack \"{}\" \"{}\"",
+                                     xnbcli_dir,
+                                     target_dir,
+                                     output_dir))
 
 
 if __name__ == '__main__':
